248e594124c35e6a4ddee0278fdba62f8aefdd13
Tim Van Den Bosch <tim_vandenbosch@msn.com>
2018-03-07 17:12:37 +0100

    [ADD] download a youtube-song
    Getting input and taking care of the download of a song

--- MusicSync/MusicSync/MainWindow.xaml.cs
+++ MusicSync/MusicSync/MainWindow.xaml.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VideoLibrary;
+using Clipboard = System.Windows.Clipboard;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
 
 namespace MusicSync
 {
@@ -20,9 +26,65 @@ namespace MusicSync
     /// </summary>
     public partial class MainWindow : Window
     {
+        private FolderBrowserDialog browseFolder;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BrowseFolder(object sender, RoutedEventArgs e)
+        {
+            browseFolder = new FolderBrowserDialog();
+            browseFolder.ShowDialog();
+            textBoxFolder.Text = browseFolder.SelectedPath;
+        }
+
+        private void PasteClipboard(object sender, RoutedEventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                if (CheckValidUrl(Clipboard.GetText()))
+                {
+                    textBoxUrl.Text = Clipboard.GetText();
+                }
+                else
+                {
+                    System.Windows.MessageBox.Show("There was no valid url found on your clipboard.");
+                }
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("There was no text found on your clipboard.");
+            }
+        }
+
+        private bool CheckValidUrl(string url)
+        {
+            try
+            {
+                var req = WebRequest.Create(url);
+                var res = req.GetResponse();
+                return true;
+            }
+            catch (WebException)
+            {
+                return false;
+            }
+            catch (UriFormatException)
+            {
+                return false;
+            }
+        }
+
+        private void SyncFolderToPlaylist(object sender, RoutedEventArgs e)
+        {
+            var url = textBoxUrl.Text;
+            var folder = textBoxFolder.Text;
+
+            var youtube = YouTube.Default;
+            var vid = youtube.GetVideo(url);
+            File.WriteAllBytes(folder, vid.GetBytes());
         }
     }
 }
